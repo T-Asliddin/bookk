cea00b2e4bd9ded511ed8ab8a3149e44730d82e0
T-Asliddin <asliddiin@gmail.com>
2024-02-05 04:08:00 +0500

    sdjcn

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-// let form = document.getElementById("form");
-// let inputName = document.getElementById("name");
-// let inputPass = document.getElementById("password");
+let form = document.getElementById("form");
+let inputName = document.getElementById("name");
+let inputPass = document.getElementById("password");
+console.log(form, inputName, inputPass);
 let searchForm = document.querySelector(".search_form");
 let search = document.querySelector(".search");
 let moreinfo = document.querySelectorAll(".info_btn");
@@ -10,30 +11,27 @@ let close_booK = document.querySelectorAll(".info_img_2");
 let list = document.querySelector(".content_list");
 let infoList = document.querySelector(".info_list");
 let content = document.querySelector(".content");
-console.log(content);
 
-// form.addEventListener("submit", (e) => {
-//   e.preventDefault();
-//   let nametest = inputName.value;
-//   let passtest = inputPass.value;
-//   let regex = /^[a-zA-Z]{4,16}$/;
-//   let resname = regex.test(nametest);
-//   let respass = regex.test(passtest);
-//   if (resname && respass) {
-//     localStorage.setItem(
-//       "name",
-//       JSON.stringify([
-//         {
-//           userName: nametest,
-//           password: passtest,
-//         },
-//       ])
-//     );
-//     document.location = "index.html";
-//   }
-// });
-
-fetch("https://openlibrary.org/people/mekBot/books/currently-reading.json")
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let nametest = inputName.value;
+  let passtest = inputPass.value;
+  let regex = /^[a-zA-Z]{4,16}$/;
+  let resname = regex.test(nametest);
+  let respass = regex.test(passtest);
+  if (resname && respass) {
+    localStorage.setItem(
+      "name",
+      JSON.stringify([
+        {
+          userName: nametest,
+          password: passtest,
+        },
+      ])
+    );
+    document.location = "index.html";
+  }
+  fetch("https://openlibrary.org/people/mekBot/books/currently-reading.json")
   .then((res) => {
     return res.json();
   })
@@ -187,3 +185,6 @@ document.addEventListener("click", (e) => {
     overla.setAttribute("class", "overla");
   }
 });
+});
+
+
